67ead7a96a8ef56655d0f1918c73d1f965696aae
Bradawk <niepceron.brad@gmail.com>
2017-09-18 23:49:45 +0200

    Display and add

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,53 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+      <!-- HEADER BAR -->
+      <div class="row header-resume">
+        <div class="col s2">
+          <small> Total of inputs </small><br>
+          <span> {{count}} </span><br>
+        </div>
+      </div>
+      <div v-if="error">
+          <h1> Error </h1>
+        </div>
+        <div v-else>
+          <div class="row main-content">
+                <div class="col s12">
+                      <form v-on:submit.prevent="addThing">     
+                        <button class="btn left waves-effect"><i class="material-icons">add</i></button>
+                      </form>
+                      <div class="Nfloat"></div>
+                      <div v-if="count">
+                        <transition-group name="slide-fade" tag="p">
+                          <div v-for="thing in things" v-bind:key="thing" class="col s3 input-div">
+                            <img class="hvr-grow" src="/static/img/thing.png" />
+                          </div>
+                        </transition-group>
+                      </div>
+                </div>
+            </div>
+        </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'index',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      count: '',
+      things: [],
+      error: ''
+    }
+  },
+  mounted(){
+    this.$http.get(process.env.API_URL)
+      .then(response => {
+        this.count = response.data.length;
+        this.things = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  },
+  methods: {
+    addThing(){
+      this.$http.post(process.env.API_URL)
+        .then(response => {
+          this.things.push(response.data.thing);
+          this.count += 1;
+          Materialize.toast(response.data.message, '3000');
+        })
+        .catch((error) =>{
+          Materialize.toast(error, '3000');
+        })
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active for <2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+  } 
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  /* Grow */
+  .hvr-grow {
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px transparent;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-property: transform;
+    transition-property: transform;
+  }
+  .hvr-grow:hover, .hvr-grow:focus, .hvr-grow:active {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 </style>
